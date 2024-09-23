ed46319dbdb82d7a04156ee2d146c597812bb696
Bhuvan <bhouvana@gmail.com>
2024-09-23 17:02:44 +0530

    static member function added

--- 1stMilestone.cpp
+++ 1stMilestone.cpp
@@ -24,17 +24,19 @@ public:
         return currentNumber;
     }
 
+    
     static int getTotalSpins() {
         return totalSpins;
     }
 };
+
 
 int RouletteWheel::totalSpins = 0;
 
 class Player {
 private:
     int balance;
-    static int totalPlayers;
+    static int totalPlayers; 
 
 public:
     Player(int initialBalance) : balance(initialBalance) {
@@ -62,31 +64,43 @@ public:
         return balance;
     }
 
+    
     static int getTotalPlayers() {
         return totalPlayers;
     }
+
+    
+    static void displayGameStats() {
+        cout << "==== Game Statistics ====" << endl;
+        cout << "Total Players: " << getTotalPlayers() << endl;
+        cout << "Total Spins: " << RouletteWheel::getTotalSpins() << endl;
+        cout << "=========================" << endl;
+    }
 };
+
 
 int Player::totalPlayers = 0;
 
 int main() {
+    
     RouletteWheel* wheel = new RouletteWheel();
     Player* player1 = new Player(1000);
     Player* player2 = new Player(1200); 
 
+    
     player1->placeBet(100, 17, *wheel);
     player2->placeBet(200, 25, *wheel);
 
+   
     cout << "Player 1 Final Balance: $" << player1->getBalance() << endl;
     cout << "Player 2 Final Balance: $" << player2->getBalance() << endl;
 
-    cout << "Total number of players: " << Player::getTotalPlayers() << endl;
-    cout << "Total number of spins: " << RouletteWheel::getTotalSpins() << endl;
+   
+    Player::displayGameStats();
 
     delete wheel;
     delete player1;
     delete player2;
-
 
     return 0;
 }
