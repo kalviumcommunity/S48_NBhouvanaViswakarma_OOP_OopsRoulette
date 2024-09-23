960a129bc4fdd409c7daa38546ad1ac3d013a512
Bhuvan <bhouvana@gmail.com>
2024-09-23 16:34:13 +0530

    static variable functionality added

--- 1stMilestone.cpp
+++ 1stMilestone.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class RouletteWheel {
 private:
     int currentNumber;
-
+    static int totalSpins; 
 
 public:
     RouletteWheel() {
@@ -17,21 +17,29 @@ public:
     void spinWheel() {
         currentNumber = rand() % 37;
         cout << "The wheel spins and lands on: " << currentNumber << endl;
+        totalSpins++; 
     }
 
     int getNumber() const {
         return currentNumber;
     }
+
+    static int getTotalSpins() {
+        return totalSpins;
+    }
 };
+
+int RouletteWheel::totalSpins = 0;
 
 class Player {
 private:
     int balance;
-
+    static int totalPlayers;
 
 public:
-    Player(int initialBalance) : balance(initialBalance) {}
-    
+    Player(int initialBalance) : balance(initialBalance) {
+        totalPlayers++; 
+    }
 
     void placeBet(int betAmount, int number, RouletteWheel& wheel) {
         if (betAmount > balance) {
@@ -50,23 +58,35 @@ public:
         }
     }
 
-
     int getBalance() const {
         return balance;
     }
+
+    static int getTotalPlayers() {
+        return totalPlayers;
+    }
 };
 
+int Player::totalPlayers = 0;
 
 int main() {
     RouletteWheel* wheel = new RouletteWheel();
-    Player* player = new Player(1000);
+    Player* player1 = new Player(1000);
+    Player* player2 = new Player(1200); 
 
-    player->placeBet(100, 17, *wheel);
+    player1->placeBet(100, 17, *wheel);
+    player2->placeBet(200, 25, *wheel);
 
-    cout << "Final Balance: $" << player->getBalance() << endl;
+    cout << "Player 1 Final Balance: $" << player1->getBalance() << endl;
+    cout << "Player 2 Final Balance: $" << player2->getBalance() << endl;
+
+    cout << "Total number of players: " << Player::getTotalPlayers() << endl;
+    cout << "Total number of spins: " << RouletteWheel::getTotalSpins() << endl;
 
     delete wheel;
-    delete player;
+    delete player1;
+    delete player2;
+
 
     return 0;
 }
